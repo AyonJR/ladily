cd6d4d479554a29f9ee3aa06d1209382839cbdd8
unknown <abdulbaset.ayon@gmail.com>
2024-11-18 21:16:30 +0600

    some sections furnished

--- src/detailsRoute/Details.jsx
+++ src/detailsRoute/Details.jsx
@@ -1,16 +1,40 @@
 import Header from "../navbar/Header";
 import Navbar from "../navbar/Navbar";
+import { SlArrowRight } from "react-icons/sl";
+import Product from "./Product";
 
 const Details = () => {
   return (
     <div>
       <Navbar></Navbar>
-      <Header></Header>
-
+      <div className="">
+        <Header></Header>
+      </div>
       {/* details content */}
-      <div className="lg:mx-[118px] mx-6  2xl:mx-[40px] mt-9">
-
-         <p>Home</p>
+      <div className="lg:px-[32px] px-6 mx-auto container   2xl:px-[40px] mt-20">
+        <div className="flex custom-font  items-center gap-[20px]">
+          <p className="font-bold text-lg">Home</p>
+          <p>
+            <SlArrowRight className="text-sm mt-1" />
+          </p>
+          <p className="font-bold text-lg">Face</p>
+          <p>
+            <SlArrowRight className="text-sm mt-1" />
+          </p>
+          <p className="font-bold text-lg">Serum</p>
+          <p>
+            <SlArrowRight className="text-sm mt-1" />
+          </p>
+          <p className="font-bold text-lg">
+            The Ordinary Retinol 0.5% Serum
+          </p>
+        </div>
+      </div>
+      <div className="lg:px-[32px] mx-auto container">
+        <hr className="w-full lg:mt-6" />
+      </div>
+      <div className="mt-4">
+        <Product></Product>
       </div>
     </div>
   );
---
+++ src/detailsRoute/Product.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Product = () => {
+    return (
+        <div className='lg:px-[32px] mx-auto container'>
+            <div className='w-full flex' >
+                <div className='w-1/2'>
+                  <img className='w-[500px]' src="https://i.ibb.co.com/RzB42Y2/0000101-the-ordinary-retinol-05-serum-415.png" alt="" />
+                </div>
+                <div className='w-1/2'>
+
+                </div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default Product;
